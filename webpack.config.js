a77769e60aac13b8f28e720599e5b68e97f0ed26
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-11-17 16:23:21 -0600

    Scripts de construccion para Produccion (Se crea webpack.dev.config.j y se ignora script de front-end)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-//Notas importantes
+// Notas importanes
 // El archivo de configuración debe usar ES5
 // Importar un administrador de rutas de archivos
 const path = require('path');
 // Exportamos un objeto de configuración
 // que sera usado por webpack
 module.exports = {
-// 0. Estableciendo el modo produccion
-mode: 'production',
+  // 0. Estableciendo el modo produccion
+  mode: 'production',
   // 1. El archivo de entrada o indexador
   entry: "./client/index.js",
   // 2. Especificar el archivo de salida
@@ -16,4 +16,32 @@ mode: 'production',
     // 2.2 Nombre del archivo de salida
     filename: "bundle.js"
   },
+  // Agregando un modulo a webpack
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    'modules': false,
+                    'useBuiltIns': 'usage',
+                    // '> 0.25%, not dead'
+                    'targets': {"chrome": 80},
+                    'corejs': 3
+                  }
+                ]
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
 }
