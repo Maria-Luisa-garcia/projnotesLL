4f4683cf593dc300fa9fbe59c7e5edc8d4ce23d4
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-11-17 18:51:43 -0600

    Se agrego Materielize CSS

--- client/index.js
+++ client/index.js
@@ -1,5 +1,12 @@
 // Cargado estilos
 import './stylesheet/myStyles.css';
 import './stylesheet/style.css';
+// Importando Estilos de Materialize CSS
+import 'materialize-css/dist/css/materialize.css';
+// Importnado scripts de Materialize
+import 'materialize-css/dist/js/materialize';
+
+// Inicializando Scripts de Materialize para interactividad
+M.AutoInit();
 
 console.log('Webpack Working Middleware!!! 📦');
