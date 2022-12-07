1f16ee1500e262f46f812f2a1608d63a5d0a47ec
maria-luisa-garcia <garciatellezmarialuisa@gmail.com>
2022-12-07 00:05:29 -0600

    Vinculacion correcta

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,7 +6,6 @@ import projectRouter from '../domains/project/projectRouter';
 const addRoutes = (app) => {
   // Agregando enrutado de Home
   app.use('/', homeRouter);
-
   // Agregando enrutado de Project
   app.use('/project', projectRouter);
 };
